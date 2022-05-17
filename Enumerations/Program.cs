badbe5a23cde48e2234d3b0a3241841817e0491c
Phu Loc <nhatminh19515@gmail.comlocchiu4216@gmail.com>
2022-05-17 15:17:10 +0700

    Add project files.

---
+++ Enumerations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enumerations
+{
+    internal class Program
+    {
+        enum Color
+        {
+            red,
+            blue=3,
+            yellow
+        }
+        static void Main(string[] args)
+        {
+           /* int myColor = (int)Color.yellow;
+            Console.WriteLine(myColor);*/
+
+            Color myColer1 = (Color)4;
+            /* Console.WriteLine(myColer1);
+             Console.ReadLine();*/
+            switch (myColer1)
+            {
+                case Color.red:
+                    Console.WriteLine("Day la mau do");
+                    break;
+                case Color.blue:
+                    Console.WriteLine("Day la mau xanh");
+                    break ;
+                case Color.yellow:
+                    Console.WriteLine("Day la mau vang");
+                    break;
+                    default: Console.WriteLine("Khong co mau nay");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
